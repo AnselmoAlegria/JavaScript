ad143b0afcfd7baa1c5e76598ccfaec750359d59
AnselmoAlegria <96200401+AnselmoAlegria@users.noreply.github.com>
2022-03-29 20:02:23 -0300

    Aula 11

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de Segurança!`)
---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brazil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil', 'Brazil') {
+    console.log(`Você é Brasileiro!`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+}
