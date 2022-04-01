ab6bce4e2230cd3ddd67e615f36e4a9203c3f1bd
AnselmoAlegria <96200401+AnselmoAlegria@users.noreply.github.com>
2022-04-01 18:40:52 -0300

    Aulas 13 e 14

---
+++ Aula13/ambiente.js
@@ -0,0 +1,21 @@
+// while (estrutura de repetição com teste no inicio)
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++    
+} 
+/* do while (estrutura de repeticação com teste no final)
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++    
+} while (c <= 6) */
+
+/*
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
